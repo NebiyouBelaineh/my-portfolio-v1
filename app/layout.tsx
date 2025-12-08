3757ae5caae3ffad27f6f81dd387ceb36fdd1e45
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-12-08 12:00:38 +0300

    chore: update dependencies in package.json
    - Bump next from ^14.2.12 to ^14.2.33
    - Upgrade nodemailer from ^6.9.15 to ^7.0.11
    - Add @next/third-parties at ^16.0.7
    - Update eslint-config-next from ^14.2.7 to ^16.0.7

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import Loading from "./loading";
-import Script from "next/script";
+// import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,23 +21,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gTag = process.env.G_TAG!;
   return (
     <html lang="en" data-theme="forest" scroll-behavior="smooth">
-       {/* Google Analytics script */}
-       <Script
-        src={"https://www.googletagmanager.com/gtag/js?id=G-RKXHVZZ2RP"}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-RKXHVZZ2RP', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
       <body className={inter.className + 'flex flex-col min-h-screen'}>
         <Header />
         <main className="p-4 container max-w-[1280px] mx-auto">
@@ -47,6 +34,7 @@ export default function RootLayout({
         </main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId={gTag}/>
     </html>
   );
 }
